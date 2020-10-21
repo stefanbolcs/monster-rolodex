e8e77a4ed0c11d08385e8200bcc7b1694714ad31
Stefan B <stefanbolcs@gmail.com>
2020-10-21 14:36:01 +0200

    Updates

---
+++ precache-manifest.928f40487a48daaf60af25d4d407c32f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b63cccf8784311dced242b26d36f141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbedc5c2e88814b0bbeb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef1034c156db6f20b704",
+    "url": "/static/js/2.9de93331.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9de93331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbedc5c2e88814b0bbeb",
+    "url": "/static/js/main.2cd76c22.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
